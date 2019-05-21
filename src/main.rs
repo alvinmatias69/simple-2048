7ddc99730f2e5444c87d112d9c7a7924254c2cd1
Matias Alvin <alvinmatias69@gmail.com>
2019-05-21 21:44:03 +0700

    Add Visual mode to the game

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,26 @@ mod game;
 mod input;
 mod interface;
 
-use self::input_interface::InputInterface;
-use self::input_string::InputString;
-use self::interface::input::{interface as input_interface, string as input_string};
+// Debug mode only
+//
+// use self::input_interface::InputInterface;
+// use self::input_string::InputString;
+// use self::interface::input::{interface as input_interface, string as input_string};
 
-use self::interface::output::{interface as output_interface, string as output_string};
-use self::output_interface::OutputInterface;
-use self::output_string::OutputString;
+// use self::interface::output::{interface as output_interface, string as output_string};
+// use self::output_interface::OutputInterface;
+// use self::output_string::OutputString;
 
 use self::game::main::Game;
 
 fn main() {
     let mut game = Game::new(4, 4);
+    game.start();
 
-    let input = InputString {};
-    let output = OutputString {};
+    // Debug mode only
+    //
+    // let input = InputString {};
+    // let output = OutputString {};
 
-    game.start(input, output);
+    // game.start_debug(input, output);
 }
