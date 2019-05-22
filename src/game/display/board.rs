fc5aceb93f5c2c262b23fb2e7fff8aee342c3738
Matias Alvin <alvinmatias69@gmail.com>
2019-05-23 02:10:25 +0700

    Refine board visual

--- src/game/display/board.rs
+++ src/game/display/board.rs
@@ -40,21 +40,46 @@ fn draw_tile(ctx: &mut Context, position: (f32, f32), value: u32) -> GameResult 
         ctx,
         graphics::DrawMode::fill(),
         graphics::Rect::new(x, y, config::TILE_SIZE, config::TILE_SIZE),
-        graphics::Color::from_rgb(238, 228, 218),
+        tile_color(value),
     )?;
     graphics::draw(ctx, &rect, graphics::DrawParam::default())?;
 
     if value > 0 {
         let text_fragment = TextFragment::new(value.to_string())
-            .color(graphics::Color::from_rgb(110, 101, 91))
+            .color(graphics::Color::from_rgb(173, 143, 135))
             .scale(graphics::Scale::uniform(config::TILE_SIZE / 2.));
-        let text = Text::new(text_fragment);
+        let mut text = Text::new(text_fragment);
+        text.set_bounds(
+            Point2::new(config::TILE_SIZE, config::TILE_SIZE),
+            graphics::Align::Center,
+        );
 
         graphics::draw(
             ctx,
             &text,
-            graphics::DrawParam::default().dest(Point2::new(x, y)),
+            graphics::DrawParam::default().dest(Point2::new(x, y + config::TILE_SIZE / 4.)),
         )?;
     }
     Ok(())
+}
+
+fn tile_color(value: u32) -> graphics::Color {
+    let rgb: Vec<u8>;
+
+    match value {
+        2 => rgb = vec![238, 236, 187],
+        4 => rgb = vec![241, 229, 179],
+        8 => rgb = vec![244, 210, 172],
+        16 => rgb = vec![242, 192, 165],
+        32 => rgb = vec![236, 183, 167],
+        64 => rgb = vec![238, 183, 180],
+        128 => rgb = vec![237, 183, 196],
+        256 => rgb = vec![236, 184, 207],
+        512 => rgb = vec![217, 178, 207],
+        1024 => rgb = vec![197, 173, 207],
+        2048 => rgb = vec![175, 167, 206],
+        _ => rgb = vec![205, 193, 181],
+    }
+
+    graphics::Color::from_rgb(rgb[0], rgb[1], rgb[2])
 }
--- src/game/display/header.rs
+++ src/game/display/header.rs
@@ -5,56 +5,83 @@ use graphics::{draw, Color, DrawParam, Mesh, Scale, Text, TextFragment};
 
 pub fn draw_title(ctx: &mut Context) -> GameResult {
     let text_fragment = TextFragment::new("2048")
-        .color(Color::from_rgb(46, 43, 39))
-        .scale(Scale::uniform(50.0));
+        .color(Color::from_rgb(97, 101, 102))
+        .scale(Scale::uniform(config::TITLE_SIZE));
     let text = Text::new(text_fragment);
 
-    draw(
-        ctx,
-        &text,
-        DrawParam::default().dest(Point2::new(config::MARGIN, config::MARGIN)),
-    )?;
+    let (x, y) = config::TITLE_POSITION;
+    draw(ctx, &text, DrawParam::default().dest(Point2::new(x, y)))?;
 
     Ok(())
 }
 
 pub fn draw_subtitle(ctx: &mut Context) -> GameResult {
     let text_fragment = TextFragment::new("Join the number and get to the 2048 tile!")
-        .color(Color::from_rgb(46, 43, 39))
-        .scale(Scale::uniform(15.0));
+        .color(Color::from_rgb(97, 101, 102))
+        .scale(Scale::uniform(config::SUBTITLE_SIZE));
     let text = Text::new(text_fragment);
 
-    draw(
-        ctx,
-        &text,
-        DrawParam::default().dest(Point2::new(config::MARGIN, 70.0)),
-    )?;
+    let (x, y) = config::SUBTITLE_POSITION;
+    draw(ctx, &text, DrawParam::default().dest(Point2::new(x, y)))?;
 
     Ok(())
 }
 
 pub fn draw_score(ctx: &mut Context, score: u32) -> GameResult {
-    let screen = graphics::screen_coordinates(ctx);
-    let box_x = screen.x + screen.w - 115.0;
-
+    let (x, y) = config::SCOREBOARD_POSITION;
     let rect = Mesh::new_rectangle(
         ctx,
         graphics::DrawMode::fill(),
-        graphics::Rect::new(box_x, config::MARGIN, 95., 45.),
+        graphics::Rect::new(x, y, config::SCOREBOARD_WIDTH, config::SCOREBOARD_HEIGHT),
         Color::from_rgb(187, 173, 160),
     )?;
     draw(ctx, &rect, DrawParam::default())?;
 
-    let text_fragment = TextFragment::new(score.to_string())
-        .color(graphics::WHITE)
-        .scale(Scale::uniform(20.));
-    let text = Text::new(text_fragment);
-    let score_x = box_x + (95. - text.width(ctx) as f32) / 2.;
+    draw_score_title(ctx)?;
+    draw_score_points(ctx, score)?;
+
+    Ok(())
+}
+
+fn draw_score_title(ctx: &mut Context) -> GameResult {
+    let text_fragment = TextFragment::new("Score")
+        .color(Color::from_rgb(173, 166, 158))
+        .scale(Scale::uniform(config::SCORE_TITLE_SIZE));
+    let mut text = Text::new(text_fragment);
+    text.set_bounds(
+        Point2::new(config::SCORE_BOUNDING.0, config::SCORE_BOUNDING.1),
+        graphics::Align::Center,
+    );
 
     draw(
         ctx,
         &text,
-        DrawParam::default().dest(Point2::new(score_x, 27.5)),
+        DrawParam::default().dest(Point2::new(
+            config::SCORE_TITLE_POSITION.0,
+            config::SCORE_TITLE_POSITION.1,
+        )),
+    )?;
+
+    Ok(())
+}
+
+fn draw_score_points(ctx: &mut Context, score: u32) -> GameResult {
+    let text_fragment = TextFragment::new(score.to_string())
+        .color(graphics::WHITE)
+        .scale(Scale::uniform(config::SCORE_SIZE));
+    let mut text = Text::new(text_fragment);
+    text.set_bounds(
+        Point2::new(config::SCORE_BOUNDING.0, config::SCORE_BOUNDING.1),
+        graphics::Align::Center,
+    );
+
+    draw(
+        ctx,
+        &text,
+        DrawParam::default().dest(Point2::new(
+            config::SCORE_POSITION.0,
+            config::SCORE_POSITION.1,
+        )),
     )?;
 
     Ok(())
